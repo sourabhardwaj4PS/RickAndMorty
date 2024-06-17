ba45380ee73ef1a359429fb6e0daa44cdb212d8c
Sourabh Bhardwaj <sourabh.bhardwaj@publicissapient.com>
2024-06-17 22:42:06 +0530

    Initial project setup and details:  - Setup of SPMs and the main app Rick & Morty  - Dependency Injection setup  - NetworkKit: Api Client implementation  - Basic views for List  - Views ready with dynamic/ protocol init()  - Unit Tests written for View Model.  - MockProtocol handled properly.  - UI Tests written WIP

---
+++ RickAndMorty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Sourabh Bhardwaj on 13/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Sourabh Bhardwaj on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
